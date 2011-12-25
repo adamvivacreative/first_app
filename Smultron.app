c78c22d99a80000937e3b987c8166077e20fdbd4
Adam Heath <adam@vivacreative.co.uk>
2011-12-25 18:41:14 +0000

    Initial commit

---
+++ Smultron.app
@@ -0,0 +1,1 @@
+/Applications/Smultron.app
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'd097d6d655567e21321ab94f094e3015c5a4fccd89736eaa0af22a7ac192ebf3ab3abd6dbbf72ca5c042f29802379c083ffef5aa6c9f8d550981d99ca52116bd'
